2a5d7d03aa346019daea423bd6541f274709bd0c
Takuto Naito <naitaku@gmail.com>
2025-02-11 23:27:50 +0900

    Use visitor pattern for DataBuilderProcessor

--- processor/src/main/java/io/github/naitaku/databuilder/processor/DataBuilderProcessor.kt
+++ processor/src/main/java/io/github/naitaku/databuilder/processor/DataBuilderProcessor.kt
@@ -13,75 +13,78 @@ class DataBuilderProcessor(
     private val logger: KSPLogger
 ) : SymbolProcessor {
 
-    override fun process(resolver: Resolver): List<KSAnnotated> {
-        return resolver.getSymbolsWithAnnotation(DataBuilder::class.qualifiedName!!)
+    override fun process(resolver: Resolver): List<KSAnnotated> =
+        resolver.getSymbolsWithAnnotation(DataBuilder::class.qualifiedName!!)
             .filterIsInstance<KSClassDeclaration>()
-            .filter { it.validate() }
-            .map { klass ->
-                if (klass.validate()) {
-                    if (!klass.modifiers.contains(Modifier.DATA)) {
-                        logger.error("@DataBuilder can only be applied to data classes", klass)
-                    } else {
-                        generateBuilderClass(klass)
-                    }
+            .map {
+                if (it.validate()) {
+                    it.accept(DataBuilderVisitor(codeGenerator, logger), Unit)
                     null
                 } else {
-                    klass
+                    it
                 }
             }.filterNotNull()
             .toList()
-    }
 
-    private fun generateBuilderClass(klass: KSClassDeclaration) {
-        val packageName = klass.packageName.asString()
-        val className = klass.simpleName.asString()
-        val builderName = "${className}Builder"
-
-        val typeSpec = TypeSpec.classBuilder(builderName)
-            .addProperty(
-                PropertySpec.builder("instance", klass.asClassName())
-                    .mutable(true)
-                    .addModifiers(KModifier.PRIVATE)
-                    .initializer("%T()", klass.asClassName())
-                    .build()
-            )
-
-        klass.primaryConstructor?.parameters?.forEach { param ->
-            if (!param.hasDefault) {
-                logger.error("All properties has to have its default value", klass)
-                return@forEach
+    internal class DataBuilderVisitor(
+        private val codeGenerator: CodeGenerator,
+        private val logger: KSPLogger
+    ) : KSVisitorVoid() {
+        override fun visitClassDeclaration(
+            classDeclaration: KSClassDeclaration,
+            data: Unit
+        ) {
+            if (!classDeclaration.modifiers.contains(Modifier.DATA)) {
+                logger.error("@DataBuilder can only be applied to data classes", classDeclaration)
+                return
             }
-            val propName = param.name?.asString() ?: return@forEach
-            val docString = klass.getAllProperties().find { it.simpleName == param.name }?.docString?.trim() ?: ""
+
+            val packageName = classDeclaration.packageName.asString()
+            val className = classDeclaration.simpleName.asString()
+            val builderName = "${className}Builder"
+
+            val typeSpec = TypeSpec.classBuilder(builderName)
+                .addProperty(
+                    PropertySpec.builder("instance", classDeclaration.toClassName())
+                        .mutable(true)
+                        .addModifiers(KModifier.PRIVATE)
+                        .initializer("%T()", classDeclaration.toClassName())
+                        .build()
+                )
+
+            classDeclaration.primaryConstructor?.parameters?.forEach { param ->
+                if (!param.hasDefault) {
+                    logger.error("All properties has to have its default value", classDeclaration)
+                    return@forEach
+                }
+                val propName = param.name?.asString() ?: return@forEach
+                val docString = classDeclaration.getAllProperties()
+                    .find { it.simpleName == param.name }?.docString?.trim() ?: ""
+
+                typeSpec.addFunction(
+                    FunSpec.builder(propName)
+                        .addKdoc(docString)
+                        .addParameter("value", param.type.toTypeName())
+                        .returns(ClassName(packageName, builderName))
+                        .addStatement("instance = instance.copy(%N = value)", propName)
+                        .addStatement("return this")
+                        .build()
+                )
+            }
 
             typeSpec.addFunction(
-                FunSpec.builder(propName)
-                    .addKdoc(docString)
-                    .addParameter("value", param.type.toTypeName())
-                    .returns(ClassName(packageName, builderName))
-                    .addStatement("instance = instance.copy(%N = value)", propName)
-                    .addStatement("return this")
+                FunSpec.builder("build")
+                    .returns(classDeclaration.toClassName())
+                    .addStatement("return instance")
                     .build()
             )
-        }
 
-        typeSpec.addFunction(
-            FunSpec.builder("build")
-                .returns(klass.asClassName())
-                .addStatement("return instance")
+            FileSpec.builder(packageName, builderName)
+                .addType(typeSpec.build())
                 .build()
-        )
-
-        FileSpec.builder(packageName, builderName)
-            .addType(typeSpec.build())
-            .build()
-            .writeTo(codeGenerator, Dependencies(true, klass.containingFile!!))
+                .writeTo(codeGenerator, Dependencies(true, classDeclaration.containingFile!!))
+        }
     }
-
-    private fun KSClassDeclaration.asClassName() = ClassName(
-        packageName.asString(),
-        simpleName.asString()
-    )
 }
 
 class DataBuilderProcessorProvider : SymbolProcessorProvider {
